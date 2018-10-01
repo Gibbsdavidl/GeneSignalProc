f2a9de14de2f1b3b2240991f7aba965cae9465f7
gibbsdavidl <gibbsdavidl@gmail.com>
2018-10-01 16:53:35 -0700

    try one big t

--- src/setScoring.py
+++ src/setScoring.py
@@ -77,8 +77,10 @@ def sampleScoringZV2( inputv ):
                 #subgraphMeanList = np.array([gsMean - np.mean(x) for x in subGraphExpr])
                 #subgraphSD = np.std( [np.std(x) for x in subGraphExpr] )
                 #zs.append( (gsMean - subgraphMean) / subgraphSD )
-                scoreList = [stats.ttest_ind(a=gsExpr, b=x, equal_var=False, nan_policy='omit')[0] for x in subGraphExpr]
-                zs.append(stats.ttest_1samp(scoreList, 0.0)[0]) # get T stat
+                #scoreList = [stats.ttest_ind(a=gsExpr, b=x, equal_var=False, nan_policy='omit')[0] for x in subGraphExpr]
+                #zs.append(stats.ttest_1samp(scoreList, 0.0)[0]) # get T stat
+                x = [item for sublist in subGraphExpr for item in sublist]
+                zs.append(stats.ttest_ind(a=gsExpr, b=x, equal_var=False, nan_policy='omit')[0])  # get T stat
 
             sampRes.append(sum(zs))
         else:
